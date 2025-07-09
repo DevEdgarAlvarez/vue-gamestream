2d489901d7627f341341bf056b3aec231534a8ff
Edgar Alvarez OKC <programador2@okcomputer.com.pe>
2025-07-09 17:35:10 -0500

    init

---
+++ src/components/Games/GameLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({
+  games: Array,
+})
+
+</script>
+
+<template>
+  <section>
+    <h2>Recent games</h2>
+    <div class="game-layout">
+
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.game-layout {
+  display: grid;
+  gap: 2rem;
+  margin: 1rem auto;
+}
+</style>
